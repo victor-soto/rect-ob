04c5b6b9b1f748edaff4d824f77507ef6b40c7fe
Martín San José de Vicente <martin@Martins-MacBook.local>
2021-11-25 16:50:00 +0100

    BabelRC configurado, Webpack server funcionando, Documentation JS genera docuemntación de los componentes en formato HTML. Creados scripts para ello

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+/**
+ * Componente APP
+ * @returns Componente principal de la aplicación
+ */
 const App = () => {
     return (
       <div>
-        <h1>Bienvenido a React</h1>
+        <h1>Bienvenido al proyecto base de React</h1>
+        <h2>Estamos usando Webpack, Babel y Eslint</h2>
       </div>
     );
 };
